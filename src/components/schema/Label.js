0882a15727ce89ca2c479f4324ad7517042b8d36
Ranjan Shrestha <ranjan@sharingapples.com>
2018-02-12 11:01:25 +0545

    Add a connected Label component

---
+++ src/components/schema/Label.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+
+const Label = (props, context) => {
+  const Component = props.as;
+  const { schema, name, field } = props;
+
+  const value = context.getSchemaValue(name);
+
+  let content = value;
+  if (schema) {
+    const state = context.store.getState();
+    const records = state.schema[schema];
+    const v = records.find(r => r.id === value);
+    if (v !== null) {
+      content = typeof field === 'function' ? field(v) : v[field];
+    }
+  }
+
+  return (
+    <Component>{content}</Component>
+  );
+};
+
+Label.propTypes = {
+  as: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.string,
+  ]),
+  name: PropTypes.string.isRequired,
+  schema: PropTypes.string,
+  field: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.string,
+  ]),
+};
+
+Label.defaultProps = {
+  as: 'p',
+  schema: null,
+  field: 'name',
+};
+
+Label.contextTypes = {
+  getSchemaValue: PropTypes.func.isRequired,
+  store: PropTypes.shape({
+    getState: PropTypes.func,
+  }).isRequired,
+};
+
+export default Label;
--- src/components/schema/index.js
+++ src/components/schema/index.js
@@ -2,10 +2,12 @@ import SchemaForm from './Form';
 import SchemaModal from './Modal';
 import withOptions from './withOptions';
 import SchemaGrid from './Grid';
+import SchemaLabel from './Label';
 
 export {
   SchemaForm,
   SchemaModal,
   SchemaGrid,
   withOptions,
+  SchemaLabel,
 };
