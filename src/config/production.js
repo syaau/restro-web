39609d6c34f62cd1c1ac36dd0f82896df4248151
Ranjan Shrestha <ranjan@sharingapples.com>
2018-02-16 11:22:38 +0545

    Add websocket url based on original host

--- src/config/production.js
+++ src/config/production.js
@@ -1,3 +1,4 @@
+/* globals window */
 export const MODE = 'production';
 export const ENDPOINT = '/';
-export const WEBSOCKET_URL = '/socket/';
+export const WEBSOCKET_URL = `ws://${window.origin.host}/socket/`;
