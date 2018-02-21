549af8c7fa7a8de503c9a4876ef9a01b533173cf
Ranjan Shrestha <ranjan@sharingapples.com>
2018-02-21 14:04:50 +0545

    Fix production configuration

--- src/config/index.js
+++ src/config/index.js
@@ -8,5 +8,15 @@ if (process.env.NODE_ENV !== 'production') {
   config = Object.assign({}, production, development);
 }
 
+export const {
+  MODE,
+  ENDPOINT,
+  WEBSOCKET_URL,
+  VAT,
+  SERVICE_CHARGE,
+  VAT_DEFAULT,
+  SERVICE_CHARGE_DEFAULT,
+} = config;
+
 // module.exports = config;
 export default config;
--- src/config/production.js
+++ src/config/production.js
@@ -1,10 +1,12 @@
 /* globals window */
 export const MODE = 'production';
 export const ENDPOINT = '/';
-export const WEBSOCKET_URL = `ws://${window.origin.host}/socket/`;
+
+// export const WEBSOCKET_URL = 'ws://localhost:8080/socket/';
+export const WEBSOCKET_URL = `ws://${window.location.host}/socket/`;
 
 export const VAT = 0.13;
 export const SERVICE_CHARGE = 0.1;
 
-export const VAT_DEFAULT = true;
+export const VAT_DEFAULT = false;
 export const SERVICE_CHARGE_DEFAULT = true;
